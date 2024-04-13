801b4817b1e85efebba4a59f2133bf5dcdc05958
MayaGerasimenko <160390629+MayaGerasimenko@users.noreply.github.com>
2024-04-13 13:30:31 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,26 +4,26 @@
 
 template<typename T, int size>
 class TStack {
-  private:
+ private:
     T* stackArray;
     int topIndex;
-  public:
+ public:
     TStack() : topIndex(-1) {
-      stackArray = new T[size];
+        stackArray = new T[size];
     }
     void pop() {
-      if (topIndex >= 0)
-        topIndex--;
+        if (topIndex >= 0)
+            topIndex--;
     }
     void push(T item) {
-      if (topIndex < size - 1)
-        stackArray[++topIndex] = item;
+        if (topIndex < size - 1)
+            stackArray[++topIndex] = item;
     }
     T get() const {
-      return stackArray[topIndex];
+        return stackArray[topIndex];
     }
     bool checkEmpty() const {
-      return topIndex == -1;
+        return topIndex == -1;
     }
 };
 
